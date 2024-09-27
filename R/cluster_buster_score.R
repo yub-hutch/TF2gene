139aba6a9542764610fa214dfb26ffb3d6a114b2
yub-hutch <142735268+yub-hutch@users.noreply.github.com>
2024-09-27 13:14:43 -0700

    Update cluster_buster_score.R

--- R/cluster_buster_score.R
+++ R/cluster_buster_score.R
@@ -1,3 +1,31 @@
-cluster_buster_score2pv <- function(motif, v) {
+load_null_cbscore <- function(motif, dir_null_cbscore) {
+  fname = file.path(dir_null_cbscore, paste0(motif, '.motifs_vs_regions.scores.feather'))
+  raw = arrow::read_feather(fname)
+  score = setNames(raw[[1]], raw$regions)
+  return(score)
+}
 
+
+load_cbscore <- function(feather) {
+  raw = arrow::read_feather(feather)
+  mat = t(as.matrix(raw[, setdiff(names(raw), 'regions')]))
+  colnames(mat) = raw$regions
+  mat = mat[, grepl("^chr([1-9]|1[0-9]|2[0-2]|X|Y):", colnames(mat))]
+  return(mat)
+}
+
+
+select_motifs <- function(cbscore, control_peaks, dir_null_cbscore, ncores) {
+  cbscore_list = sapply(rownames(cbscore), simplify = F, function(motif) cbscore[motif, ])
+  do.call(rbind, pbmcapply::pbmcmapply(function(motif, score) {
+    # Set matched null cluster-buster score
+    all_null_score = load_null_cbscore(motif, dir_null_cbscore)
+    matched_null_score = all_null_score[control_peaks]
+
+    # Fold change & P-value
+    logfc = log(mean(score) / mean(matched_null_score))
+    pv = wilcox.test(score, matched_null_score, alternative = 'greater')$p.value
+
+    dplyr::tibble(motif = motif, logfc = logfc, pv = pv)
+  }, names(rep1_cbscore_list), rep1_cbscore_list, SIMPLIFY = F, mc.cores = ncores))
 }
