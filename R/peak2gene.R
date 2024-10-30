d5717c1c7ab08dc01ec6030fdf2f0a1b9f20bae7
yub-hutch <142735268+yub-hutch@users.noreply.github.com>
2024-10-29 18:02:31 -0700

    Add TF2peak.R

--- R/peak2gene.R
+++ R/peak2gene.R
@@ -1,53 +1,63 @@
-#' Calculate Peak to Gene Distance
+#' Calculate the distance between consensus peaks to genes
 #'
-#' This function calculates the distance between the center of peaks and the nearest gene transcription start site (TSS)
+#' This function calculates the distance between the center of peaks and the nearest TSS,
 #' if the distance is less than a specified maximum distance.
 #'
-#' @param peak A tibble containing peak data with columns: 'chr', 'start', and 'end'.
-#' @param ncores An integer specifying the number of cores to use for parallel processing.
+#' @param consensus_peak A tibble containing consensus peak data with columns: 'chr', 'start', and 'end'.
+#' @param ncores Number of cores to use for parallel processing.
 #' @param meta_gene A tibble containing gene metadata with columns: 'id', 'symbol', 'biotype', 'chr', 'start', 'end', 'strand', 'tss' (default is grch38).
-#' @param max_distance A numeric value specifying the maximum distance to consider (default is 500,000).
-#' @param verbose A logical value indicating whether to print progress messages (default is TRUE).
+#' @param max_distance Maximum distance to consider (default is 500,000).
 #'
-#' @return A tibble with columns: 'gene', 'peak', and 'distance'.
+#' @return A sparse matrix representing the distances. To distinguish distance 0 from empty sparse matrix
+#' elements, distance 0 is set to 0.1.
 #' @export
 #'
-calc_peak2gene_distance <- function(peak, ncores, meta_gene = grch38, max_distance = 500e3, verbose = TRUE) {
+calc_peak2gene_distance <- function(consensus_peak, ncores, meta_gene = grch38, max_distance = 500e3) {
   # Calculate peak center & name
-  peak = peak %>%
+  message('Formatting inputs ...')
+  consensus_peak = consensus_peak %>%
     dplyr::mutate(center = (start + end) / 2) %>%
-    dplyr::mutate(name = paste0('chr', chr, ':', start, '-', end))
+    dplyr::mutate(name = paste0(chr, ':', start, '-', end))
 
-  # Calculate the distance between centers of peaks & nearest gene TSS if < max_distance
+  # Calculate the distance between peak centers & nearest TSS if < max_distance
   df = NULL
-  for (chr in unique(meta_gene$chr)) {
-    if (verbose) message(paste0('Working on chr ', chr, '...'))
-
-    meta_gene_chr = meta_gene[meta_gene$chr == chr, ]
-    peak_chr = peak[peak$chr == chr, ]
-
+  for (chr in unique(consensus_peak$chr)) {
+    message(paste0('Working on ', chr, '...'))
+    meta_gene_chr = meta_gene[meta_gene$chr == strsplit(chr, 'chr')[[1]][2], ]
+    consensus_peak_chr = consensus_peak[consensus_peak$chr == chr, ]
     df_chr = do.call(rbind, parallel::mcmapply(function(gene) {
       curr_meta_gene = meta_gene_chr %>% dplyr::filter(id == gene)
       res = NULL
       for (tss in curr_meta_gene$tss) {
-        distances = abs(peak_chr$center - tss)
+        distances = abs(consensus_peak_chr$center - tss)
         selected = distances < max_distance
         if (any(selected)) {
-          res = rbind(res, dplyr::tibble(peak = peak_chr$name[selected], distance = distances[selected]))
+          res = rbind(res, dplyr::tibble(
+            consensus_peak = consensus_peak_chr$name[selected],
+            distance = distances[selected]
+          ))
         }
       }
-      if (is.null(res)) {
-        return(NULL)
-      } else {
-        res = res %>%
-          dplyr::group_by(peak) %>%
-          dplyr::summarise(distance = min(distance), .groups = 'drop')
-        return(dplyr::tibble(gene = gene, res))
-      }
+      if (is.null(res)) return(NULL)
+      res %>%
+        dplyr::group_by(consensus_peak) %>%
+        dplyr::summarise(distance = min(distance), .groups = 'drop') %>%
+        dplyr::mutate(gene = gene)
     }, unique(meta_gene_chr$id), SIMPLIFY = F, mc.cores = ncores))
-
     df = rbind(df, dplyr::tibble(chr = chr, df_chr))
   }
 
-  return(df)
+  # Create sparse matrix
+  message('Creating sparse matrix ...')
+  consensus_peaks = unique(df$consensus_peak)
+  genes = unique(df$gene)
+  mat = Matrix::sparseMatrix(
+    i = match(df$consensus_peak, consensus_peaks),
+    j = match(df$gene, genes),
+    x = pmax(df$distance, 0.1),
+    dims = c(length(consensus_peaks), length(genes)),
+    dimnames = list(consensus_peaks, genes)
+  )
+
+  return(mat)
 }
