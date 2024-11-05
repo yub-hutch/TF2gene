f81bef859825f44132cc6ee36b27736de5909c22
yub-hutch <142735268+yub-hutch@users.noreply.github.com>
2024-11-04 18:46:36 -0800

    Update select_active_motifs.R

--- R/select_active_motifs.R
+++ R/select_active_motifs.R
@@ -7,6 +7,12 @@
 #' @return Tibble with `motif` and `prop_enrich` columns.
 #' @export
 select_active_motifs <- function(trans_cbscore) {
-  prop_enrich = 1 - 2 * rowMeans(trans_cbscore > 0.5)
-  dplyr::tibble(motif = names(prop_enrich), prop_enrich = prop_enrich)
+  thrs = seq(0.5, 0.9, by = 0.1)
+  props_enrich = sapply(thrs, function(thr) {
+    prop_above_thr = rowMeans(trans_cbscore > thr)
+    prop_below_thr_under_null = thr / (1 - thr) * prop_above_thr
+    1 - prop_above_thr - prop_below_thr_under_null
+  })
+  min_prop_enrich = apply(props_enrich, 1, min)
+  dplyr::tibble(motif = names(min_prop_enrich), min_prop_enrich = min_prop_enrich)
 }
