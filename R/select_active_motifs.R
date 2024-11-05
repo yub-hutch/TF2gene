21af882da6097a2d96b8b2cb15c8137f93bc7d83
yub-hutch <142735268+yub-hutch@users.noreply.github.com>
2024-11-04 16:53:45 -0800

    Update select_active_motifs.R

--- R/select_active_motifs.R
+++ R/select_active_motifs.R
@@ -1,82 +1,30 @@
-#' Select Active Motifs Based on Cluster-Buster Scores
+#' Cauchy Combination Test
 #'
-#' This function selects active motifs by comparing their cluster-buster scores on consensus peaks and matched control regions.
+#' This function combines \( p \)-values using the Cauchy Combination Test, which is effective
+#' for handling dependencies among tests.
 #'
-#' @param cbscore A matrix of cluster-buster scores of consensus peaks.
-#' @param control_regions A names vector of best control regions for each consensus peak.
-#' See \code{\link{extract_best_matched_control_region}}.
-#' @param dir_null_cbscore A character string representing the directory containing the cluster-buster scores on control regions.
-#' @param ncores An integer specifying the number of cores to use for parallel processing.
-#' @param dir_out A character string representing the directory to save the results.
-#' @return This function is called by its side effect.
-#' @export
-select_active_motifs_with_cbscore <- function(cbscore, control_regions, dir_null_cbscore, ncores, dir_out) {
-  if (!dir.exists(dir_out)) dir.create(dir_out)
-
-  if (!(setequal(names(control_regions), colnames(cbscore)) &
-        (ncol(cbscore) == length(control_regions)))) stop("cbscore & control_regions don't match")
-
-  # Run
-  message('Preparing to run ...')
-  cbscore_list = sapply(rownames(cbscore), simplify = F, function(motif) cbscore[motif, ])
-  rm(cbscore)
-  gc()
-
-  unfinished_motifs = names(cbscore_list)
-  message(paste0(length(unfinished_motifs), ' motifs detected'))
-
-  # Compare
-  message('Comparing Cluster-Buster scores on consensus peaks & matched control regions ...')
-  while (length(unfinished_motifs) > 0) {
-    junk = tryCatch({
-      pbmcapply::pbmcmapply(function(motif, score) {
-        # Set matched null cluster-buster score
-        all_null_score = read_null_cbscore(motif, dir_null_cbscore)
-        matched_null_score = all_null_score[control_regions]
-
-        # Fold change, P-value, & AUC
-        logfc = log(mean(score) / mean(matched_null_score))
-        pv = wilcox.test(score, matched_null_score)$p.value
-        auc = as.numeric(pROC::auc(
-          response = c(rep(1, length(score)), rep(0, length(matched_null_score))),
-          predictor = c(score, matched_null_score),
-          levels = c(0, 1),
-          direction = '<'
-        ))
-        res = dplyr::tibble(motif = motif, logfc = logfc, pv = pv, auc = auc)
-
-        # Save
-        saveRDS(res, file = file.path(dir_out, paste0(motif, '.rds')))
-
-        return(NULL)
-      }, names(cbscore_list), cbscore_list, SIMPLIFY = F, mc.cores = ncores)
-    }, error = function(e) 'unfinished')
-
-    finished_motifs = sapply(strsplit(list.files(dir_out), '.rds'), head, n = 1)
-    unfinished_motifs = setdiff(names(cbscore_list), finished_motifs)
-
-    if (length(unfinished_motifs) > 0) {
-      message(paste0(length(unfinished_motifs), ' motifs unfinished. Retry ...'))
-      cbscore_list = cbscore_list[unfinished_motifs]
-    } else {
-      message('done.')
-    }
-  }
-  return(NULL)
+#' @param pvals Numeric vector of p-values (each between 1e-16 and 1).
+#' @return Combined p-value from the Cauchy test.
+cauchy_combination_test <- function(pvals) {
+  stopifnot(all(!is.na(pvals)) & all(pvals > 1e-16) & all(pvals < 1))
+  weights = rep(1 / length(pvals), length(pvals))
+  stat = sum(weights * tan((0.5 - pvals) * pi))
+  ifelse(stat > 1e15, 1 / stat / pi, 1 - pcauchy(stat))
 }
 
 
-#' Collect Motif Selection Results
+#' Select Active Motifs Based on Transformed Cluster-Buster Scores
 #'
-#' This function collects outputs of \link{\code{select_active_motifs_with_cbscore}} from a specified directory.
+#' This function applies the Cauchy Combination Test to transformed Cluster-Buster scores and
+#' returns a tibble of motifs and combined p-values.
 #'
-#' @param dir_out A character string specifying the directory containing the RDS files.
-#' @param ncores An integer specifying the number of cores to use for parallel processing.
-#'
-#' @return A tibble with columns motif, logfc, pv, and auc.
-#' @export
-collect_motif_selection <- function(dir_out, ncores) {
-  do.call(rbind, parallel::mcmapply(function(file) {
-    readRDS(file.path(dir_out, file))
-  }, list.files(dir_out), SIMPLIFY = F, mc.cores = ncores))
+#' @param trans_cbscore Matrix of transformed Cluster-Buster scores (rows are motifs).
+#' @param n_control Numeric, the number of control regions used.
+#' @return A tibble with motifs and their combined p-values.
+select_active_motifs <- function(trans_cbscore, n_control) {
+  epsilon = 0.1 / n_control
+  trans_cbscore[trans_cbscore == 0] = epsilon
+  trans_cbscore[trans_cbscore == 1] = 1 - epsilon
+  pvals = apply(trans_cbscore, 1, cauchy_combination_test)
+  dplyr::tibble(motif = rownames(trans_cbscore), pv = pvals)
 }
